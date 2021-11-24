57a37687852ef7c5827fe42839324fd46719dcb1
Balakrishna1308 <91240572+Balakrishna1308@users.noreply.github.com>
2021-11-24 02:23:33 -0500

    First commit

---
+++ Second.c
@@ -0,0 +1,20 @@
+# include <stdio.h>
+# include <conio.h>
+
+int main()
+{
+  
+    int num;
+  //clrscr();
+  printf("Enter a number: ");
+  scanf("%d",&num);
+
+  printf("Given number is ");
+
+  printf("%d",num);
+
+  printf("\nThis is end of thhe program, thank you");
+  
+  return 0;
+
+}
